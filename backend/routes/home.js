a9bda98120bda158a852d725cb8915d64ffca463
Farhan Daemi <fdmxfarhan@yahoo.com>
2021-11-14 11:08:13 +0330

    asdf

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -2,12 +2,9 @@ var express = require('express');
 var router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 const {convertDate} = require('../config/dateConvert');
-// var pdf = require("pdf-creator-node");
-var pdf = require('html-pdf');
-
+var pdf = require("pdf-creator-node");
 var path = require('path');
 var fs = require('fs');
-
 
 
 router.get('/', (req, res, next) => {
@@ -20,39 +17,45 @@ router.get('/form', (req, res, next) => {
 
 router.post('/form', (req, res, next) => {
     var {fullname, accountNumber, maximum, idNumber} = req.body;
-    fs.readFile('public/t.html', 'utf8', (err, html) => {
+    // fs.readFile('./public/t.html', 'utf8', (err, html) => {
+    // html = fs.createReadStream('./public/t.html');
         var options = {
             format: "A4",
+            orientation: "portrait",
+            border: "5mm",
+            header: {
+                height: "0",
+                contents: ''
+            },
+            footer: {
+                height: "0mm",
+                contents: {}
+            }
         };
-        pdf.create(html, options).toFile('./public/businesscard.pdf', function(err, res2) {
-            if (err) return console.log(err);
-            console.log(res2); // { filename: '/app/businesscard.pdf' }
-            res.send("<a href='/businesscard.pdf' target='_blank'>دانلود</a>")
-        });
-        // var document = {
-        //     html: html,
-        //     data: {
-        //         info: {
-        //             fullname: fullname,
-        //             accountNumber: accountNumber,
-        //             maximum: maximum,
-        //             idNumber: idNumber,
-        //             date: convertDate(new Date()),
-        //             formNumber: 1,
-        //         }
-        //     },
-        //     path: "public/output.pdf",
-        //     type: "",
-        // };
+        var document = {
+            html: '<p>html</p>',
+            data: {
+                // info: {
+                //     fullname: fullname,
+                //     accountNumber: accountNumber,
+                //     maximum: maximum,
+                //     idNumber: idNumber,
+                //     date: convertDate(new Date()),
+                //     formNumber: 1,
+                // }
+            },
+            path: "public/output.pdf",
+            type: "",
+        };
     
-        // pdf.create(document, options)
-        //     .then((r) => {
-        //         res.send("<a href='/output.pdf' target='_blank'>دانلود</a>")
-        //     })
-        //     .catch((error) => {
-        //         console.error(error);
-        //     });
-    });
+        pdf.create(document, options)
+            .then((r) => {
+                res.send("<a href='/output.pdf' target='_blank'>دانلود</a>")
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    // });
 
 })
 

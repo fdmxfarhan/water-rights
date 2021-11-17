85118d1ae2e43a06071d88b8e2c4058bb70fc6e1
Farhan Daemi <fdmxfarhan@yahoo.com>
2021-11-17 16:17:36 +0330

    asdf

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -6,9 +6,110 @@ var pdf = require("pdf-creator-node");
 var path = require('path');
 var fs = require('fs');
 var phantomjs = require('phantomjs');
+var Transmission = require('../models/Transmission');
+var Acount = require('../models/Acount');
+var User = require('../models/User');
 
 router.get('/', (req, res, next) => {
     res.render('home');
+});
+
+router.get('/forms', ensureAuthenticated, (req, res, next) => {
+    var {transmissionID} = req.query;
+    Transmission.findById(transmissionID, (err, transmission) => {
+        Acount.findById(transmission.source._id, (err, source) => {
+            Acount.findById(transmission.target._id, (err, target) => {
+                User.findById(source.ownerID, (err, sourceUser) => {
+                    User.findById(target.ownerID, (err, targetUser) => {
+                        fs.readFile('./public/form1.html', 'utf8', (err, form1) => {
+                            fs.readFile('./public/form2.html', 'utf8', (err, form2) => {
+                                fs.readFile('./public/form3.html', 'utf8', (err, form3) => {
+                                    var options = {
+                                        phantomPath: path.join(__dirname, '../node_modules/phantomjs/lib/phantom/bin/phantomjs'),
+                                        // phantomPath: '/usr/local/share/phantomjs-1.9.8-linux-x86_64/bin/phantomjs',
+                                        format: "A3",
+                                        orientation: "portrait",
+                                        border: "5mm",
+                                        header: {
+                                            height: "0",
+                                            contents: ''
+                                        },
+                                        footer: {
+                                            height: "0mm",
+                                            contents: {}
+                                        },
+                                    };
+                                    var document1 = {
+                                        html: form1,
+                                        data: {
+                                            info: {
+                                                fullname: source.owner,
+                                                accountNumber: source.type == 'chah'? source.license : source.accountNumber,
+                                                maximum: source.charge + transmission.amount,
+                                                idNumber: sourceUser.idNumber,
+                                                date: convertDate(new Date()),
+                                                formNumber: 1,
+                                            }
+                                        },
+                                        path: 'public/files/form1.pdf',
+                                        type: "",
+                                    };
+                                    var document2 = {
+                                        html: form2,
+                                        data: {
+                                            info: {
+                                                fullname: target.owner,
+                                                accountNumber: target.type == 'chah'? target.license : target.accountNumber,
+                                                maximum: target.charge + transmission.amount,
+                                                idNumber: targetUser.idNumber,
+                                                date: convertDate(new Date()),
+                                                formNumber: 1,
+                                                amount: transmission.amount
+                                            }
+                                        },
+                                        path: 'public/files/form2.pdf',
+                                        type: "",
+                                    };
+                                    var document3 = {
+                                        html: form3,
+                                        data: {
+                                            info: {
+                                                fullname: source.owner,
+                                                accountNumber: source.type == 'chah'? source.license : source.accountNumber,
+                                                maximum: source.charge + transmission.amount,
+                                                idNumber: sourceUser.idNumber,
+                                                date: convertDate(new Date()),
+                                                formNumber: 1,
+                                                amount: transmission.amount,
+                                                form1Number: 1,
+                                                form1Date: convertDate(new Date()),
+                                                form2Number: 1,
+                                                form2Date: convertDate(new Date()),
+                                                endDate: `${source.startDate.year}/${source.startDate.month}/${source.startDate.day}`,
+                                                sourceAccountNum: source.type == 'chah'? source.license : source.accountNumber,
+                                                targetOwner: target.owner,
+                                                targetOwnerID: target.type == 'chah'? target.license : target.accountNumber,
+                                                
+                                            }
+                                        },
+                                        path: 'public/files/form3.pdf',
+                                        type: "",
+                                    };
+                                    pdf.create(document1, options).then((r) => {
+                                        pdf.create(document2, options).then((r) => {
+                                            pdf.create(document3, options).then((r) => {
+                                                res.render('forms');
+                                            }).catch((error) => {console.error(error)});
+                                        }).catch((error) => {console.error(error)});
+                                    }).catch((error) => {console.error(error)});
+                                });
+                            });
+                        });
+                    })
+                })
+            });
+        });
+    });
 });
 
 router.get('/form', (req, res, next) => {
